9d794fa4d5262482fa650c1c625dcdac210c5d19
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2022-04-04 00:10:15 +0300

    ...
    ...

--- character.py
+++ character.py
@@ -1,7 +1,19 @@
+import os, os.path
+import pygame
+
 class Player:
 
-    def __init__(self, name_of_charakter):
-        pass
+    def __init__(self, name_of_charakter, path_to_sprite):
+        self.up_list = []
+        self.down_list = []
+        self.left_list = []
+        self.right_list = []
+        for i in range(os.listdir(path_to_sprite+'\Up')):
+            image = pygame.image.load(path_to_sprite+'\Up'+str(i)).convert()
+            self.up_list.append(image)
+        print('Done')
+
+
 
     
 
--- main.py
+++ main.py
@@ -2,35 +2,48 @@ import pygame
 import sys 
 import pyautogui
 from character import Player
-
+p = pygame
 width, height = pyautogui.size()
 
 #######################################     Itizialithation
-pygame.init() 
+p.init() 
 
 size = (width, height) 
-screen = pygame.display.set_mode(size) 
-color = (0, 0, 0) 
+screen = p.display.set_mode(size) 
+black = (0, 0, 0) 
+screen.fill(black)
   
+######################################      Functions
+
+p.draw.rect(screen, (250, 0, 0), p.Rect(width-20, 0, width, 20))
+player = Player(name_of_charakter='Baky', path_to_sprite='./sprites/Baky')
+
+
 
 #######################################     Running
 running = True
 while running:           
-    for event in pygame.event.get():
+    for event in p.event.get():
 
         if event.type == pygame.QUIT:
             running = False
 
         if running == False:
-            pygame.quit()
-
+            p.quit()
+        
         else:
-            pass
+
+            if event.type == p.MOUSEBUTTONDOWN:
+                pos = p.mouse.get_pos()
+                if pos[0] >= width-20 and pos[0] <= width and pos[1] >= 0 and pos[1] <= 20:
+                    pygame.quit()
 
 
-                  
-      
 
-      
-    pygame.display.update() 
+
+                    
+        
+
+            pygame.display.flip()
+            pygame.display.update() 
 
