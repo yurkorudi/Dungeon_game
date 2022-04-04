b95eaf9d2337661338c43a860c836d056d28802a
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2022-04-04 15:09:53 +0200

    1
    Not_working

--- character.py
+++ character.py
@@ -1,19 +1,78 @@
 import os, os.path
 import pygame
+from tkinter import *
+t = Tk()
 
 class Player:
-
-    def __init__(self, name_of_charakter, path_to_sprite):
+# Sates: stand, stand_left, stand_right, go_left, go_right, go_up, go_down
+#
+#
+    def __init__(self, name_of_charakter, path_to_sprite, x, y):
+        self.STATE = 'stand'
+        self.y = y
+        self.x = x
+        self.step = 5
         self.up_list = []
         self.down_list = []
         self.left_list = []
         self.right_list = []
+        self.stand_list = []
         for i in range(len(os.listdir(path_to_sprite+'/Up'))):
-            image = pygame.image.load(path_to_sprite+'/Up'+str(i+1)+'.gif').convert()
+            image = pygame.image.load(path_to_sprite+'/Up/'+str(i+1)+'.gif').convert()
+            image = pygame.transform.scale(image, (200, 200))
+            image.set_colorkey((255, 255, 255))
             self.up_list.append(image)
-        print('Done')
+        print('Done with: ', self.up_list)
+
+        for i in range(len(os.listdir(path_to_sprite+'/Down'))):
+            image = pygame.image.load(path_to_sprite+'/Down/'+str(i+1)+'.gif').convert()
+            image = pygame.transform.scale(image, (200, 200))
+            image.set_colorkey((255, 255, 255))
+            self.down_list.append(image)
+        print('Done with: ', self.down_list)
+
+        for i in range(len(os.listdir(path_to_sprite+'/Left'))):
+            image = pygame.image.load(path_to_sprite+'/Left/'+str(i+1)+'.gif').convert()
+            image = pygame.transform.scale(image, (200, 200))
+            image.set_colorkey((255, 255, 255))
+            self.left_list.append(image)
+        print('Done with: ', self.left_list)
+
+        for i in range(len(os.listdir(path_to_sprite+'/Right'))):
+            image = pygame.image.load(path_to_sprite+'/Right/'+str(i+1)+'.gif').convert()
+            image = pygame.transform.scale(image, (200, 200))
+            image.set_colorkey((255, 255, 255))
+            self.right_list.append(image)
+        print('Done with: ', self.right_list)
+
+        for i in range(len(os.listdir(path_to_sprite+'/Stand'))):
+            image = pygame.image.load(path_to_sprite+'/Stand/'+str(i+1)+'.gif').convert()
+            image = pygame.transform.scale(image, (200, 200))
+            image.set_colorkey((255, 255, 255))
+            self.stand_list.append(image)
+        print('Done with: ', self.stand_list)
 
 
+        self.image = self.stand_list[2]
 
+    def draw(self, where):
+        where.blit(self.image, (self.x-(self.image.get_width()/2), self.y-(self.image.get_height()/2)))
+        
+    def change_sprites(self):
+        pass
+
+    def go_left(self):
+        self.STATE = 'go_left'
+        self.x = self.x - self.step
+
+
+    def go_right(self):
+        pass
+
+    def go_up(self):
+        pass
+
+    def go_down(self):
+        pass
     
 
--- main.py
+++ main.py
@@ -9,20 +9,24 @@ width, height = pyautogui.size()
 p.init() 
 
 size = (width, height) 
-screen = p.display.set_mode(size) 
-black = (0, 0, 0) 
+screen = pygame.display.set_mode(size) 
+screen.set_alpha(128)
+black = (0, 255, 0) 
 screen.fill(black)
   
 ######################################      Functions
+def button1():
+    p.draw.rect(screen, (250, 0, 0), p.Rect(width-20, 0, width, 20))
 
-p.draw.rect(screen, (250, 0, 0), p.Rect(width-20, 0, width, 20))
-player = Player(name_of_charakter='Baky', path_to_sprite='./sprites/Baky')
+
+player = Player(name_of_charakter='Baky', path_to_sprite='./sprites/Baky', x=width/2, y=height/2)
 
 
 
 #######################################     Running
 running = True
-while running:           
+while running:
+    p.time.delay(10)           
     for event in p.event.get():
 
         if event.type == pygame.QUIT:
@@ -37,6 +41,20 @@ while running:
                 pos = p.mouse.get_pos()
                 if pos[0] >= width-20 and pos[0] <= width and pos[1] >= 0 and pos[1] <= 20:
                     pygame.quit()
+                    sys.exit()
+
+            keys = pygame.key.get_pressed()
+
+            if event.type == p.KEYDOWN:
+                if event.key == p.K_LEFT:
+                    player.x = player.x - player.step
+                
+            if event.type == p.KEYUP:
+                pass
+
+            screen.fill(black)
+            player.draw(screen)
+            button1()
 
 
 
